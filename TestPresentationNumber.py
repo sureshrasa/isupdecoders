84bcac6de48ef5603e6c76f6462be2522cf165a7
Suresh Rasakulasuriar <suresh@suriar.me.uk>
2018-06-05 20:43:24 +0100

    Fixed unit test

--- TestPresentationNumber.py
+++ TestPresentationNumber.py
@@ -19,14 +19,14 @@ class TestPresentationNumber(unittest.TestCase):
         self.assertTrue(PresentationNumber(StreamDecoder.fromBits("0xfd0d, 0b1, 0b1010001, 0x12345678")).isOddAddressSignals)
 
     def testScreeningIndicator(self):
-        self.assertEqual(PresentationNumber(StreamDecoder.fromBits("0xfd0da3, 0b00, 0b11, 0x1234567")).screeningIndicator, 0b11)
-        self.assertEqual(PresentationNumber(StreamDecoder.fromBits("0xfd0da3, 0b00, 0b10, 0x1234567")).screeningIndicator, 0b10)
-        self.assertEqual(PresentationNumber(StreamDecoder.fromBits("0xfd0da3, 0b00, 0b00, 0x1234567")).screeningIndicator, 0b0)
+        self.assertEqual(PresentationNumber(StreamDecoder.fromBits("0xfd0da3, 0b0000, 0b0011, 0x123456")).screeningIndicator, 0b11)
+        self.assertEqual(PresentationNumber(StreamDecoder.fromBits("0xfd0da3, 0b0000, 0b0010, 0x123456")).screeningIndicator, 0b10)
+        self.assertEqual(PresentationNumber(StreamDecoder.fromBits("0xfd0da3, 0b0000, 0b0000, 0x123456")).screeningIndicator, 0b0)
 
     def testAddressPresentationRestricted(self):
-        self.assertEqual(PresentationNumber(StreamDecoder.fromBits("0xfd0da3, 0b11, 0b10, 0x1234567")).addressPresentationRestricted, 0b11)
-        self.assertEqual(PresentationNumber(StreamDecoder.fromBits("0xfd0da3, 0b10, 0b11, 0x1234567")).addressPresentationRestricted, 0b10)
-        self.assertEqual(PresentationNumber(StreamDecoder.fromBits("0xfd0da3, 0b00, 0b11, 0x1234567")).addressPresentationRestricted, 0b00)
+        self.assertEqual(PresentationNumber(StreamDecoder.fromBits("0xfd0da3, 0b0000, 0b1100, 0x123456")).addressPresentationRestricted, 0b11)
+        self.assertEqual(PresentationNumber(StreamDecoder.fromBits("0xfd0da3, 0b0000, 0b1000, 0x123456")).addressPresentationRestricted, 0b10)
+        self.assertEqual(PresentationNumber(StreamDecoder.fromBits("0xfd0da3, 0b0000, 0b0000, 0x123456")).addressPresentationRestricted, 0b00)
 
 if __name__ == '__main__':
     unittest.main()
