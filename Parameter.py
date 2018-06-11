97a25db1836a11102f2dbc8e7095839dbdd968fa
Suresh Rasakulasuriar <suresh@suriar.me.uk>
2018-06-11 11:37:46 +0100

    Updated parameter to respect length indicator encoding being optional

--- Parameter.py
+++ Parameter.py
@@ -3,12 +3,7 @@ from ByteStream import *
 class Parameter:
     def __init__(self, decoder):
         self._code = decoder.readField(8)
-        self._length = decoder.readField(8)
 
     @property
     def code(self):
         return self._code
-
-    @property
-    def length(self):
-        return self._length
