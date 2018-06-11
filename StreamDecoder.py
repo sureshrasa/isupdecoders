23bbfbb489b7ab1a707be735e132036e101126a1
Suresh Rasakulasuriar <suresh@suriar.me.uk>
2018-06-11 11:19:49 +0100

    Added support for spanning byte boundaries

--- StreamDecoder.py
+++ StreamDecoder.py
@@ -3,9 +3,6 @@
 #
 #  Copyright: © Resilientplc.com Ltd. 2018 - All Rights Reserved
 #
-
-from bitstring import BitArray
-
 class StreamDecoder:
 
     def __init__(self, payload):
@@ -13,12 +10,20 @@ class StreamDecoder:
         self._currByteIndex = 0
         self._currBitIndex = 0 
         self._currBits = payload[0] 
-        self._prevLen = 0     
+        self._prevLen = 0    
 
-    def readField(self, length):
+    def skipBytes(self, len):
+        if (self._currBitIndex%8 != 0):
+            raise Exception("Cannot skip mid bytes", self._currByteIndex, self._currBitIndex)
+
+        self._currByteIndex += len
+        self._currBits = self._payload[self._currByteIndex]
+        self._prevLen = 0    
+
+    def _readField(self, length):
         if (self._currBitIndex == 8):
             if (self._currByteIndex >= len(self._payload)):
-                raise Exception("Stream reached eof", self._currByteIndexIndex)
+                raise Exception("Stream reached eof", self._currByteIndex)
 
             self._currBitIndex = 0
             self._currByteIndex +=1
@@ -39,5 +44,53 @@ class StreamDecoder:
  
         return result;
 
-    def fromBits(bitString):
-        return StreamDecoder(bytes.fromhex(BitArray(bitString).hex))
+    def hasMore(self):
+        return self._currBitIndex < 8 or self._currByteIndex < (len(self._payload)-1)
+
+    def readField(self, length):
+        if length <= self._currBitIndex:
+            return self._readField(length)
+
+        result = 0
+        lsbLen = 0
+        
+        bitsLeft = 8 - self._currBitIndex
+        if (bitsLeft > 0) and (length > bitsLeft):
+            #print("Reading bits; len=", bitsLeft)
+            result = self._readField(bitsLeft)
+            length -= bitsLeft
+            lsbLen = bitsLeft
+
+        while length > 8:
+            #print("Reading bytes; len=", length)
+            result += self._readField(8)<<lsbLen
+            length -= 8
+            lsbLen += 8
+
+        if length > 0:
+            #print("Reading last bits; len=", length)
+            result += self._readField(length)<<lsbLen
+
+        return result
+
+class StreamDecoderSlice:
+    def __init__(self, stream, size):
+        self._stream = stream
+        self._bitLen = size * 8
+
+    def skipBytes(self, len):
+        self._stream.skipBytes(len)
+        self._bitLen -= len * 8
+
+    def readField(self, length):
+        if (self._bitLen < length):
+            raise Exception("Stream reached eof", self._bitLen, length)
+
+        result = self._stream.readField(length)
+        self._bitLen -= length
+
+        return result
+
+    def hasMore(self):
+        return self._bitLen > 0
+
---
+++ TestStreamDecoder.py
@@ -0,0 +1,88 @@
+import unittest
+from StreamDecoder import *
+
+from bitstring import BitArray
+
+class TestDecoder:
+    def test1Bit(self):
+        stream = self.fromBits("0b1101, 0b0101");
+        self.assertTrue(stream.hasMore())
+        self.assertEqual(stream.readField(1), 1)
+        self.assertTrue(stream.hasMore())
+        self.assertEqual(stream.readField(1), 0)
+        self.assertTrue(stream.hasMore())
+        self.assertEqual(stream.readField(1), 1)
+        self.assertTrue(stream.hasMore())
+        self.assertEqual(stream.readField(1), 0)
+        self.assertTrue(stream.hasMore())
+        self.assertEqual(stream.readField(1), 1)
+        self.assertTrue(stream.hasMore())
+        self.assertEqual(stream.readField(1), 0)
+        self.assertTrue(stream.hasMore())
+        self.assertEqual(stream.readField(1), 1)
+        self.assertTrue(stream.hasMore())
+        self.assertEqual(stream.readField(1), 1)
+        self.assertFalse(stream.hasMore())
+
+    def test4Bits(self):
+        stream = self.fromBits("0xA, 0x3, 0x2, 0xE, 0x5, 0xC,");
+        self.assertTrue(stream.hasMore())
+        self.assertEqual(stream.readField(4), 0x3)
+        self.assertTrue(stream.hasMore())
+        self.assertEqual(stream.readField(4), 0xA)
+        self.assertTrue(stream.hasMore())
+        self.assertEqual(stream.readField(4), 0xE)
+        self.assertTrue(stream.hasMore())
+        self.assertEqual(stream.readField(4), 0x2)
+        self.assertTrue(stream.hasMore())
+        self.assertEqual(stream.readField(4), 0xC)
+        self.assertTrue(stream.hasMore())
+        self.assertEqual(stream.readField(4), 0x5)
+        self.assertFalse(stream.hasMore())
+
+    def test431Bits(self):
+        stream = self.fromBits("0b1101, 0b0101");
+        self.assertEqual(stream.readField(4), 0b0101)
+        self.assertEqual(stream.readField(3), 0b101)
+        self.assertEqual(stream.readField(1), 0b1)
+        self.assertFalse(stream.hasMore())
+
+    def testByte(self):
+        stream = self.fromBits("0xAB, 0xCD, 0x01, 0x52");
+        self.assertEqual(stream.readField(8), 0xAB)
+        self.assertEqual(stream.readField(8), 0xCD)
+        self.assertEqual(stream.readField(8), 0x01)
+        self.assertEqual(stream.readField(8), 0x52)
+        self.assertFalse(stream.hasMore())
+
+    def testWordOnByteBoundary(self):
+        stream = self.fromBits("0xAB, 0xCD, 0x01, 0x52");
+        self.assertEqual(stream.readField(16), 0xCDAB)
+        self.assertEqual(stream.readField(16), 0x5201)
+        self.assertFalse(stream.hasMore())
+  
+    def testLongWordOnByteBoundary(self):
+        stream = self.fromBits("0xABCD0152, 0x89ABCDEF");
+        self.assertEqual(stream.readField(32), 0x5201CDAB)
+        self.assertEqual(stream.readField(32), 0xEFCDAB89)
+        self.assertFalse(stream.hasMore())
+      
+    def testWordAcrossByteBoundary(self):
+        stream = self.fromBits("0b110001, 0b10, 0b00010100, 0b00101010, 0b11, 0b100101");
+        self.assertEqual(stream.readField(2),  0b10)
+        self.assertEqual(stream.readField(14), 0b00010100110001)
+        self.assertEqual(stream.readField(14), 0b10010100101010)
+        self.assertEqual(stream.readField(2),  0b11)
+        self.assertFalse(stream.hasMore())
+
+class TestStreamDecoder(TestDecoder, unittest.TestCase):
+    def fromBits(self, bitString):
+        return StreamDecoder(bytes.fromhex(BitArray(bitString).hex))
+
+class TestStreamDecoderSlice(TestDecoder, unittest.TestCase):
+    def fromBits(self, bitString):
+        data = bytes.fromhex(BitArray(bitString + ", 0x1234").hex)
+        return StreamDecoderSlice(StreamDecoder(data), len(data) - 2)
+
+if __name__ == '__main__':
+    unittest.main()
